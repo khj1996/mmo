5b2619bdbb26eb8f131661e240e9d018aae73c57
test <test>
2024-11-09 03:31:24 +0900

    플레이어 걷기 달리기 점프 애니메이션 수정

--- Ggaammee/Ggaammee/Assets/InputSystem/InputSystem.cs
+++ Ggaammee/Ggaammee/Assets/InputSystem/InputSystem.cs
@@ -1,77 +1,70 @@
 using UnityEngine;
-#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
-#endif
 
-	public class InputSystem : MonoBehaviour
-	{
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+public class InputSystem : MonoBehaviour
+{
+    [Header("Character Input Values")] public Vector2 move;
+    public Vector2 look;
+    public bool jump;
+    public bool sprint;
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
+    [Header("Movement Settings")] public bool analogMovement;
 
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
-
-#if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
-		{
-			MoveInput(value.Get<Vector2>());
-		}
-
-		public void OnLook(InputValue value)
-		{
-			if(cursorInputForLook)
-			{
-				LookInput(value.Get<Vector2>());
-			}
-		}
-
-		public void OnJump(InputValue value)
-		{
-			JumpInput(value.isPressed);
-		}
-
-		public void OnSprint(InputValue value)
-		{
-			SprintInput(value.isPressed);
-		}
-#endif
+    [Header("Mouse Cursor Settings")] public bool cursorLocked = true;
+    public bool cursorInputForLook = true;
 
 
-		public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
+    public void OnMove(InputValue value)
+    {
+        MoveInput(value.Get<Vector2>());
+    }
 
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
+    public void OnLook(InputValue value)
+    {
+        if (cursorInputForLook)
+        {
+            LookInput(value.Get<Vector2>());
+        }
+    }
 
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
+    public void OnJump(InputValue value)
+    {
+        JumpInput(value.isPressed);
+    }
 
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
-		
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
+    public void OnSprint(InputValue value)
+    {
+        SprintInput(value.isPressed);
+    }
 
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
-	}
-	
+
+    public void MoveInput(Vector2 newMoveDirection)
+    {
+        move = newMoveDirection;
+    }
+
+    public void LookInput(Vector2 newLookDirection)
+    {
+        look = newLookDirection;
+    }
+
+    public void JumpInput(bool newJumpState)
+    {
+        jump = newJumpState;
+    }
+
+    public void SprintInput(bool newSprintState)
+    {
+        sprint = newSprintState;
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        SetCursorState(cursorLocked);
+    }
+
+    private void SetCursorState(bool newState)
+    {
+        Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+}
