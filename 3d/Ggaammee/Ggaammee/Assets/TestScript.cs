28bc6bc70ca855dde473ef77c2d2ec478a78f833
test <test>
2025-01-15 03:49:23 +0900

    범위 진행도를 위한 쉐이더 그래프 수정

---
+++ 3d/Ggaammee/Ggaammee/Assets/TestScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class TestScript : MonoBehaviour
+{
+    private DecalProjector test;
+
+    public void tttt()
+    {
+        test.mode
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
