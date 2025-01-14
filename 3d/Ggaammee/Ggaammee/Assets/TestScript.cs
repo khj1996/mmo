f967cbb8d37dd21eb79deecf150d4048b1170e6e
test <test>
2025-01-15 06:08:57 +0900

    원형 공격범위 오브젝트 추가

--- 3d/Ggaammee/Ggaammee/Assets/TestScript.cs
+++ 3d/Ggaammee/Ggaammee/Assets/TestScript.cs
@@ -5,22 +5,26 @@ using UnityEngine.Rendering.Universal;
 
 public class TestScript : MonoBehaviour
 {
-    private DecalProjector test;
+    private DecalProjector fullRange;
+    private DecalProjector progressRange;
+    private MaterialPropertyBlock propertyBlock;
 
     public void tttt()
-    {
+    {/*
+        propertyBlock = new MaterialPropertyBlock();
+        fullRange.material.get(propertyBlock);
+        propertyBlock.SetFloat("_Progress", progress); // 쉐이더의 프로퍼티 이름이 "_Progress"일 경우
+        objRenderer.SetPropertyBlock(propertyBlock);*/
     }
-    
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
